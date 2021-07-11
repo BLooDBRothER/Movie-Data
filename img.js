4c1a6bf4ae63d809bbcf031ef4e822c30a39dc18
BLooDBRothER <computertechnoloy446@gmail.com>
2021-07-11 08:51:11 +0530

    Animation Bug fix

--- img.js
+++ img.js
@@ -3,31 +3,35 @@
 let count = 0;
 const imgs = document.querySelectorAll(".thumb-img");
 
+const img3 = window.matchMedia("(max-width: 1600px)");
+const img2 = window.matchMedia("(max-width: 1040px)");
+
 async function updateArray(){
     let response = await fetch(popularurl);
     let result = await (response.json());
     popularMovie = result.results;
     popularMovie.forEach(movie => {
         imgarr.push(imgurl+movie.poster_path);
+        console.log(movie.title);
     });
-    hideImage3(img3);
-    hideImage2(img2);
+    hideImage3(img3, 0);
+    hideImage2(img2, 0);
     updateImage();
 }
 
-function hideImage3(x) {
+function hideImage3(x, val) {
     if (x.matches) {
       imgs[2].style.display = "none";
-    } else {
+    } else if(val){
       imgs[2].style.display = "initial";
       updateImageSrc(imgs[2]);
     }
   }
   
-  function hideImage2(x) {
+  function hideImage2(x, val) {
     if (x.matches) {
       imgs[1].style.display = "none";
-    } else {
+    } else if(val){
       imgs[1].style.display = "initial";
       updateImageSrc(imgs[1]);
     }
@@ -37,31 +41,31 @@ function updateImageSrc(img) {
   if (count == imgarr.length) {
     count = 0;
   }
+  console.log(count);
   img.src = imgarr[count];
-  img.style.animation = "fadeIn 1s ease 1";
+  // img.style.animation = "fadeIn 1s ease 1";
+  img.classList.remove("transparent");
   count++;
 }
 
 function updateImage() {
   imgs.forEach((img) => {
     if (img.style.display == "none") return;
+    console.log(img);
     updateImageSrc(img);
-    setTimeout(() => {
-      img.style.animation = "fadeOut 1s ease 1";
-    }, 4000);
-    img.addEventListener("animationend", function(e){
-        if(e.animationName == "fadeIn") return;
-        updateImage();
-    });
   });
-//   setTimeout(updateImage, 5000);
+  setTimeout(() => {
+    imgs.forEach((img) => {
+        img.classList.add("transparent");
+    });
+  }, 4000);
+  setTimeout(updateImage, 5000);
 }
 
 
 updateArray();
 
-var img3 = window.matchMedia("(max-width: 1600px)");
-var img2 = window.matchMedia("(max-width: 1040px)");
 
-img3.addEventListener("change", hideImage3);
-img2.addEventListener("change", hideImage2);
+
+img3.addEventListener("change", () => {hideImage3(img3, 1)});
+img2.addEventListener("change", () => {hideImage2(img2, 1)});
